7bc3ce5ddc9375c044f7d67e28a3472b53c874dd
Laurenz1606 <laurenz.rausche@gmail.com>
2022-11-17 12:20:46 +0000

    serve frontend

--- src/Components/express.ts
+++ src/Components/express.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import { join } from "path";
+import { cwd } from "process";
+import { ApiRouter } from "../Routers/ApiRouter";
 import { logger } from "./logger";
 
 //create new express instance
 export const app = express();
+
+//use routers
+app.use("/api", ApiRouter);
+
+//serve frontend
+app.get("/*", (req, res) => {
+  res.sendFile(join(cwd(), "frontend", "build", "index.html"));
+});
 
 //log that express app was created
 logger.log("debug", "Express-App created!");
